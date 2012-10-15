acc30597ec8816d5d881ee557c689a202d78fe53
Georg Ringer <georg.ringer@gmail.com>
2012-10-15 20:56:34 +0200

    start of bootstrap form

--- Resources/Private/TypoScript/content/tt_content.ts
+++ Resources/Private/TypoScript/content/tt_content.ts
@@ -149,3 +149,103 @@ lib.stdheader.3.headerClass.cObject.25 {
 		isInList.field = layout
 	}
 }
+
+
+
+/* still in alpha/beta */
+tt_content.mailform.20 {
+  stdWrap.wrap >
+  postProcessor.1.messages.success = TEXT
+  postProcessor.1.messages.success {
+    value = <strong>Thanks for your email</strong><br />I will try to responst as soon as possible!
+    wrap = <div class="alert alert-success">|</div>
+  }
+
+  layout {
+    form (
+    <form class="form-horizontal">
+      <containerWrap />
+    </form>
+    )
+    containerWrap (
+      <div>
+    <elements />
+    </div>
+
+    )
+    elementWrap (
+    <div class="">
+      <element />
+    </div>
+    )
+    mandatory (
+    <span class="required">
+      *
+    </span>
+    )
+    error (
+    <strong class="ym-message">
+      <errorvalue />
+    </strong>
+    )
+    textline (
+    <div class="control-group">
+      <span class="control-label"><label class="bar" /></span>
+      <div class="controls"><input class="fo" /></div><inputvalue />
+    </div>
+    )
+    textarea (
+    <div class="control-group">
+      <span class="control-label"><label class="bar" /></span>
+      <div class="controls"><textarea /></div><inputvalue />
+    </div>
+    )
+    select (
+    <div class="control-group">
+      <label />
+      <select>
+        <elements />
+      </select>
+    </div>
+    )
+    checkbox (
+     <div class="control-group">
+      <span class="control-label"><label /></span>
+      <div class="controls"><input /></div>
+    </div>
+    )
+    radio (
+   <div class="control-group">
+      <span class="control-label"><label /></span>
+      <div class="controls"><input /></div>
+    </div>
+    )
+    button (
+    <div class="ym-fbox-button">
+      <input />
+    </div>
+    )
+    reset (
+    <div class="ym-fbox-button">
+      <input />
+    </div>
+    )
+    submit (
+    <div class="form-actions">
+      <input class="btn" />
+    </div>
+    )
+    radiogroup(
+    <div class="control-group subgroup">
+     <span class="control-label"><legend/></span>
+      <div class="controls"><containerWrap /></div>
+    </div>
+    )
+    checkboxgroup(
+    <div class="control-group subgroup">
+     <span class="control-label"><legend/></span>
+      <div class="controls"><containerWrap /></div>
+    </div>
+    )
+  }
+}

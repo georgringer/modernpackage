3f857d562fb6b4522674a103e25c8e7b0394906a
Georg Ringer <georg.ringer@gmail.com>
2012-10-15 12:56:24 +0200

    Change lib.content_XXX to lib.content.XXX

--- Resources/Private/TypoScript/lib/content.ts
+++ Resources/Private/TypoScript/lib/content.ts
@@ -14,48 +14,48 @@
 #-------------------------------------------------------------------------------
 #	CONTENT: Main Content (colPos = 0)
 #-------------------------------------------------------------------------------
-lib.content_main = COA
-lib.content_main {
+lib.content.main = COA
+lib.content.main {
 	stdWrap.wrap = <div class="content-main-inner">|</div>
 	10 < styles.content.get
 }
-lib.content_0 < lib.content_main
+lib.content.0 < lib.content.main
 
 #-------------------------------------------------------------------------------
 #	CONTENT: Left Content (colPos = 1)
 #-------------------------------------------------------------------------------
-lib.content_left = COA
-lib.content_left {
+lib.content.left = COA
+lib.content.left {
 	stdWrap.wrap = <div class="content-left-inner">|</div>
 	10 < styles.content.getLeft
 }
-lib.content_1 < lib.content_left
+lib.content.1 < lib.content.left
 
 #-------------------------------------------------------------------------------
 #	CONTENT: Right Content (colPos = 2)
 #-------------------------------------------------------------------------------
-lib.content_right = COA
-lib.content_right {
+lib.content.right = COA
+lib.content.right {
 	stdWrap.wrap = <div class="content-right-inner">|</div>
 	10 < styles.content.getRight
 }
-lib.content_2 < lib.content_right
+lib.content.2 < lib.content.right
 
 #-------------------------------------------------------------------------------
 #	CONTENT: Border Content (colPos = 3)
 #-------------------------------------------------------------------------------
-lib.content_border = COA
-lib.content_border {
+lib.content.border = COA
+lib.content.border {
 	stdWrap.wrap = <div class="content-border-inner">|</div>
 	10 < styles.content.getBorder
 }
-lib.content_3 < lib.content_border
+lib.content.3 < lib.content.border
 
 #-------------------------------------------------------------------------------
 #	CONTENT: colPos = 4
 #-------------------------------------------------------------------------------
-lib.content_4 = COA
-lib.content_4 {
+lib.content.4 = COA
+lib.content.4 {
 	stdWrap.wrap = <div class="content-border-inner">|</div>
 	10 < styles.content.get
 	10.select.where = colPos=4
@@ -64,8 +64,8 @@ lib.content_4 {
 #-------------------------------------------------------------------------------
 #	CONTENT: colPos = 5
 #-------------------------------------------------------------------------------
-lib.content_5 = COA
-lib.content_5 {
+lib.content.5 = COA
+lib.content.5 {
 	stdWrap.wrap = <div class="content-border-inner">|</div>
 	10 < styles.content.get
 	10.select.where = colPos=5
@@ -74,8 +74,8 @@ lib.content_5 {
 #-------------------------------------------------------------------------------
 #	CONTENT: colPos = 6
 #-------------------------------------------------------------------------------
-lib.content_6 = COA
-lib.content_6 {
+lib.content.6 = COA
+lib.content.6 {
 	stdWrap.wrap = <div class="content-border-inner">|</div>
 	10 < styles.content.get
 	10.select.where = colPos=6

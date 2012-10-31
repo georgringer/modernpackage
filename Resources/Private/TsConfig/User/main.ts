34223063af88358704f2ece9616b9641213a09bf
Georg Ringer <georg.ringer@gmail.com>
2012-10-31 08:11:13 +0100

    [FEATURE] Add page tree UserTsConfig
    Improve the usability by setting some nice
    defaults

--- Resources/Private/TsConfig/User/main.ts
+++ Resources/Private/TsConfig/User/main.ts
@@ -5,3 +5,23 @@
 #	GNU General Public License as published by the Free Software Foundation;
 #	either version 2 of the License, or at your option) any later version.
 # ******************************************************************************
+
+options.pageTree {
+	# Shows the user db mount path above the mount itself (useful if you work a lot with user db mounts)
+	showPathAboveMounts = 1
+
+	# Shows the page id as a prefix of the title
+	showPageIdWithTitle = 1
+
+	# Shows the navigation title instead of the title if available
+	showNavTitle = 1
+
+	# Disables the left click on an icon that opens the contextmenu
+	disableIconLinkToContextmenu = 0
+
+	# Disable the filter feature in the top panel
+	hideFilter = 1
+
+	# Shows the domain name as a suffix of the title if available
+	showDomainNameWithTitle = 1
+}

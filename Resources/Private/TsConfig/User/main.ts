289d59a759a809a04a3849056887355d1a632d9b
Georg Ringer <georg.ringer@gmail.com>
2012-10-31 08:39:21 +0100

    [TASK] Fix a typo

--- Resources/Private/TsConfig/User/main.ts
+++ Resources/Private/TsConfig/User/main.ts
@@ -3,7 +3,7 @@
 #
 #	You can redistribute it and/or modify it under the terms of the
 #	GNU General Public License as published by the Free Software Foundation;
-#	either version 2 of the License, or at your option) any later version.
+#	either version 2 of the License, or (at your option) any later version.
 # ******************************************************************************
 
 # General

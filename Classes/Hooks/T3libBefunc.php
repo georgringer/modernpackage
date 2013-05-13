0882a8dfcc75f80d22178f32144bb8043d94428b
Georg Ringer <georg.ringer@gmail.com>
2013-05-13 15:26:14 +0200

    [FEATURE] Change class selection for tables to a select field

---
+++ Classes/Hooks/T3libBefunc.php
@@ -0,0 +1,65 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Georg Ringer <georg.ringer@cyberhouse.at>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+class Tx_Modernpackage_Hooks_T3libBefunc {
+
+	/**
+	 * Hook function of t3lib_befunc
+	 * It is used to change the flexform rendering
+	 *
+	 * @param array &$dataStructure Flexform structure
+	 * @param array $conf some strange configuration
+	 * @param array $row row of current record
+	 * @param string $table table name
+	 * @param string $fieldName some strange field name
+	 * @return void
+	 */
+	public function getFlexFormDS_postProcessDS(&$dataStructure, $conf, $row, $table, $fieldName) {
+		if ($table === 'tt_content' && $row['CType'] === 'table' && is_array($dataStructure)) {
+			$this->updateFlexforms($dataStructure, $row);
+		}
+	}
+
+	/**
+	 * Update flexform configuration if a action is selected
+	 *
+	 * @param array|string &$dataStructure flexform structure
+	 * @param array $row row of current record
+	 * @return void
+	 */
+	protected function updateFlexforms(array &$dataStructure, array $row) {
+		$dataStructure['sheets']['sDEF']['ROOT']['el']['acctables_tableclass']['TCEforms']['config'] = array(
+			'type' => 'select',
+			'items' => array(
+				array('LLL:EXT:modernpackage/Resources/Private/Language/locallang_be.xml:tableclasses.default', 'table'),
+				array('LLL:EXT:modernpackage/Resources/Private/Language/locallang_be.xml:tableclasses.striped', 'table table-striped'),
+			),
+			'size' => 1,
+			'maxitems' => 1,
+		);
+	}
+
+}
+
+?>
--- ext_localconf.php
+++ ext_localconf.php
@@ -29,4 +29,8 @@ if (!empty($settings['setUserTSconfig'])) {
 	t3lib_extMgm::addUserTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Resources/Private/TsConfig/User/main.ts">');
 }
 
+	// Modify flexform values
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_befunc.php']['getFlexFormDSClass'][$_EXTKEY] =
+	'EXT:' . $_EXTKEY . '/Classes/Hooks/T3libBefunc.php:Tx_Modernpackage_Hooks_T3libBefunc';
+
 ?>

f463b8708103401c110b87b852e7ce25449892bc
Georg Ringer <georg.ringer@gmail.com>
2013-05-13 11:13:31 +0200

    [TASK] Change dev/prod settings

--- Resources/Private/TypoScript/constants-development.ts
+++ Resources/Private/TypoScript/constants-development.ts
@@ -7,31 +7,9 @@
 # ******************************************************************************
 
 
-# **********************************************************
+# *******************************************************************
 # Constants of the DEVELOPMENT mode
-# **********************************************************
+# !!! add everything to the production.ts and only those things
+#     which differ to this section!
+# *******************************************************************
 
-plugin.theme_configuration.company = Modern Package
-plugin.theme_configuration.popup = &copy; - Modern Package
-plugin.theme_configuration.title = Modern Package
-plugin.theme_configuration.url = http://localhost/master/
-
-
-plugin.theme_configuration.navigation_service = 232
-plugin.theme_configuration.navigation_service-right = 238
-plugin.theme_configuration.navigation_languageswitch.languages = 0,1,2
-plugin.theme_configuration.navigation_languageswitch.labels = English |*| German |*| Italian
-plugin.theme_configuration.navigation.footer-right = 1,254,274,250
-plugin.theme_configuration.navigation.footer-left = 249
-plugin.theme_configuration.general.copyright_information.link = 242
-
-plugin.theme_configuration.assets.merge = 0
-plugin.theme_configuration.assets.compress = 0
-
-plugin.theme_configuration.extensions.indexed_search.form-target = 241
-plugin.theme_configuration.extensions.news.latest {
-	limit = 3
-	detailPid = 259
-	startingpoint = 260
-	cropMaxCharacters = 120
-}

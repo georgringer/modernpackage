deab1d511772e826aedaa4d1e471bc4b3852bef6
Georg Ringer <georg.ringer@gmail.com>
2013-05-17 08:10:57 +0200

    [TASK] Fix flexslider

--- Resources/Private/Extensions/rgsmoothgallery.ts
+++ Resources/Private/Extensions/rgsmoothgallery.ts
@@ -17,20 +17,6 @@
 includeLibs.rgsmoothgallery = typo3conf/ext/rgsmoothgallery/Classes/Rendering/ConfigurationRendering.php
 
 tt_content.image.20 {
-	# Set some classes around
-	imageStdWrap.dataWrap.override {
-		if.isTrue.field = tx_rgsmoothgallery_rgsg
-		cObject = COA
-		cObject {
-			10 = TEXT
-			10.value (
-				<div class="flexslider" id="slideshow">	<ul class="slides">| </ul></div>
-			)
-			20 = TEXT
-			20.value =
-		}
-	}
-
 	# Add the JS configuration
 	#imageStdWrap.stdWrap.append = USER
 	#imageStdWrap.stdWrap.append {
@@ -38,44 +24,21 @@ tt_content.image.20 {
 	#	userFunc = Tx_Rgsmoothgallery_Rendering_ConfigurationRendering->user_renderConfiguration
 	#}
 
-	# Override the title field and use the caption
-	1.altText.field.override = imagecaption
-	1.altText.field.override.if.isTrue.field = tx_rgsmoothgallery_rgsg
-	1.titleText.field.override = imagecaption
-	1.titleText.field.overrideif.isTrue.field = tx_rgsmoothgallery_rgsg
-
-	# Don't render the caption if not needed
-	caption.stdWrap.if.isFalse.field = tx_rgsmoothgallery_rgsg
-
 	# Render just the image
-	rendering.rgsmoothgallery {
-		allStdWrap {
-			wrap = <div class="csc-textpic-imagewrap">|</div>
-			innerWrap.cObject = CASE
-			innerWrap.cObject {
-				key.field = imageorient
-				# above-center
-				0 = TEXT
-				0.value = <div class="csc-textpic-center-outer"><div class="csc-textpic-center-inner">|</div></div>
-				# below-center
-				8 < .0
-			}
-		}
-		oneImageStdWrap {
-			wrap = <li>|</li>
+	rendering.rgsmoothgallerycaption {
+		allStdWrap.wrap = <div class="flexslider" id="slideshow"><ul class="slides">|</ul></div>
+		singleStdWrap {
+			wrap = <li>|###CAPTION###</li>
 		}
 
-		imgTagStdWrap {
-			wrap =|
-		}
 		caption {
 			required = 1
-			wrap = <div class="caption">|</div>
+			wrap = <p class="flex-caption">|</p>
 		}
 	}
 
 	renderMethod.stdWrap {
-		override = rgsmoothgallery
+		override = rgsmoothgallerycaption
 		override.if.isTrue.field = tx_rgsmoothgallery_rgsg
 	}
 }

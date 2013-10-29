6bf1a9fa3d237c5d026b26eee277e30c10aee369
Georg Ringer <georg.ringer@gmail.com>
2013-10-29 08:57:49 +0100

    [TASK] Remove copyrights

--- Resources/Private/TsConfig/Page/news-other-records.ts
+++ Resources/Private/TsConfig/Page/news-other-records.ts
@@ -1,2 +1,3 @@
+
 # to be included by using <INCLUDE_TYPOSCRIPT: source="FILE:EXT:modernpackage/Resources/Private/TsConfig/Page/news-other-records.ts">
 mod.web_list.allowedNewTables = tx_news_domain_model_category,tx_news_domain_model_tag,sys_note

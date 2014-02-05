a2551478bf70babfb34b70f3e237b522acf02e07
Georg Ringer <georg.ringer@gmail.com>
2014-02-05 08:52:08 +0100

    [FEATURE] Clear Systemcache item for test+dev

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,6 +8,12 @@ if (!defined('TYPO3_MODE')) {
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Resources/Private/TsConfig/Page/main.ts">');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Resources/Private/TsConfig/User/main.ts">');
 
+// Add menu item to clear system cache for Development & Testing context
+$context = \TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext()->__toString();
+if ($context === 'Development' || $context === 'Testing') {
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('options.clearCache.system = 1');
+}
+
 // Modify flexform values
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_befunc.php']['getFlexFormDSClass'][$_EXTKEY] =
 	'GeorgRinger\\Modernpackage\\Hooks\T3libBefunc';

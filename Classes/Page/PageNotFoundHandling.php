20f4f94431828a4d058674bcb76f84732b2234f3
Georg Ringer <georg.ringer@gmail.com>
2014-03-04 08:42:32 +0100

    [TASK] Multilanguage page not found handling

--- Classes/Page/PageNotFoundHandling.php
+++ Classes/Page/PageNotFoundHandling.php
@@ -2,28 +2,28 @@
 
 namespace GeorgRinger\Modernpackage\Page;
 
-/***************************************************************
- *  Copyright notice
- *
- *  (c) 2014 Georg Ringer <georg.ringer@cyberhouse.at>
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+	/***************************************************************
+	 *  Copyright notice
+	 *
+	 *  (c) 2014 Georg Ringer <georg.ringer@cyberhouse.at>
+	 *  All rights reserved
+	 *
+	 *  This script is part of the TYPO3 project. The TYPO3 project is
+	 *  free software; you can redistribute it and/or modify
+	 *  it under the terms of the GNU General Public License as published by
+	 *  the Free Software Foundation; either version 2 of the License, or
+	 *  (at your option) any later version.
+	 *
+	 *  The GNU General Public License can be found at
+	 *  http://www.gnu.org/copyleft/gpl.html.
+	 *
+	 *  This script is distributed in the hope that it will be useful,
+	 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 *  GNU General Public License for more details.
+	 *
+	 *  This copyright notice MUST APPEAR in all copies of the script!
+	 ***************************************************************/
 
 /**
  * Class PageNotFoundHandling
@@ -55,10 +55,46 @@ class PageNotFoundHandling {
 
 		if (!is_null($errorPageUid)) {
 			$link = $domain . 'index.php?id=' . $errorPageUid;
+
+			$sysLanguageUid = $this->getSysLanguage($ref);
+			if ($sysLanguageUid > 0) {
+				$link .= '&L=' . $sysLanguageUid;
+			}
+
 			\TYPO3\CMS\Core\Utility\HttpUtility::redirect($link, \TYPO3\CMS\Core\Utility\HttpUtility::HTTP_STATUS_404);
 		}
 
 		$message = 'The page not found handling could not handle the request. The original message was: "' . $params['reasonText'] . '" with URL "' . $params['currentUrl'] . '"';
 		throw new \TYPO3\CMS\Core\Error\Http\PageNotFoundException($message, 1301648780);
+	}
+
+
+	/**
+	 * Try to get the current sys_language from various places
+	 *
+	 * @param \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController $ref
+	 * @return int
+	 */
+	protected function getSysLanguage(\TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController $ref) {
+		if ($ref->sys_language_uid > 0) {
+			return (int)$ref->sys_language_uid;
+		}
+
+		$getRequest = (int)GeneralUtility::_GET('L');
+		if ($getRequest > 0) {
+			return $getRequest;
+		}
+
+		$realurlVars = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT']['preVars'];
+		$firstUrlArgument = array_shift(GeneralUtility::trimExplode('/', GeneralUtility::getIndpEnv('REQUEST_URI'), TRUE));
+		if ($firstUrlArgument) {
+			foreach ($realurlVars as $mapping) {
+				if ($mapping['GETvar'] === 'L' && isset($mapping['valueMap'][$firstUrlArgument])) {
+					return (int)$mapping['valueMap'][$firstUrlArgument];
+				}
+			}
+		}
+
+		return 0;
 	}
 }

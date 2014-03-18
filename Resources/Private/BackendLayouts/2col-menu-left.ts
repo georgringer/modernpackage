4e31debdc953c91d7f6d87ad2ff81ecff75e8ccc
Georg Ringer <georg.ringer@gmail.com>
2014-03-18 09:46:07 +0100

    change 3col to 2col-menu-left

--- Resources/Private/BackendLayouts/2col-menu-left.ts
+++ Resources/Private/BackendLayouts/2col-menu-left.ts
@@ -1,19 +1,15 @@
 backend_layout {
-	colCount = 4
+	colCount = 3
 	rowCount = 1
 	rows {
 		1 {
 			columns {
 				1 {
-					name = LLL:EXT:cms/locallang_ttc.xlf:colPos.I.0
-					colPos = 1
-				}
-				2 {
 					name = LLL:EXT:cms/locallang_ttc.xlf:colPos.I.1
 					colspan = 2
 					colPos = 0
 				}
-				3 {
+				2 {
 					name = LLL:EXT:cms/locallang_ttc.xlf:colPos.I.2
 					colPos = 2
 				}
